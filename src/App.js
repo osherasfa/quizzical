ff30a1c760edb9a744753300233b3e726747e46f
osher <osherasfa@gmail.com>
2022-07-31 14:03:53 +0300

    V2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Welcome from './components/Welcome';
 import Questions from './components/Questions';
 
 export default function App() {
+  const [questions, setQuestions] = React.useState([])
   const [coninute, setConinute] = React.useState(false)
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
+    .then((res) => res.json())
+    .then((data) => setQuestions(data.results))
+}, [])
+
   return (
     <div className="App">
       <div className='quizzical'>
-        {coninute ? <Questions/> :<Welcome nextPage={() => setConinute(true)}/>}
+        {coninute ? <Questions questions={questions}/> :<Welcome nextPage={() => setConinute(true)}/>}
       </div>
     </div>
   );
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,34 +1,50 @@
 import React from "react";
 
-export default function Questions() {
-    const [questions, setQuestions] = React.useState([
-        {question:"blah blah?", rightAnswer:"blah!", answers: ["lol", "hehe:)", "blah!", "oh.."]},
-        {question:"what's my name?", rightAnswer:"Osher", answers: ["Diago", "Osher", "Eyal", "Fredi"]}])
+export default function Questions(props) {
+    const { questions } = props
+    const [checkedList, setCheckedList] = React.useState(Array(questions.length).fill(-1))
+    console.log(checkedList)
+
+    function handleClick(value, index){
+        setCheckedList(prevCheckedList => Object.values({...prevCheckedList, [index]: value}))
+    }
     
-    const questionsElements = questions.map(quest => { 
-        return <div className="quiz-container">
+    function createAnswers(incorrect_answers, correct_answer, parentIndex){
+        const randomizeAnswers = []
+        const randomIndex = Math.floor(Math.random() * 4)
+        let incorrectCouner = 0
+        
+        for (let i = 0; i < incorrect_answers.length + 1; i++) {
+            if(randomIndex === i)
+                randomizeAnswers.push(<div onClick={() => handleClick(i, parentIndex)}
+                className={"answer " + (checkedList[parentIndex] === i ? "checked": "") }
+                key={i}>{correct_answer}</div>)
+            else{
+                randomizeAnswers.push(<div onClick={() => handleClick(i, parentIndex)}
+                className={"answer " + (checkedList[parentIndex] === i ? "checked": "") }
+                key={i}>{incorrect_answers[incorrectCouner]}</div>)
+                incorrectCouner += 1
+            }
+        }
+        return randomizeAnswers
+    }
+
+    const questionsElements = questions.map((quest, index) => { 
+        return <div className="quiz-container" key={index}>
             <h1 className="question">{quest.question}</h1>
-            {quest.answers.map((answer, index) =>{
-                return <React.Fragment>
-                    <input
-                    className="quiz-answer"
-                    type="radio"
-                    id={answer}
-                    name={quest.question}
-                    value={answer}
-                    checked={true}
-                    onChange={()=>{}}
-                    />
-                    <label htmlFor={answer}>{answer}</label>
-                </React.Fragment>
-            })}
-            <br />
+
+            <div className="answers-container">
+                {createAnswers(quest.incorrect_answers, quest.correct_answer, index)}                
+            </div>
+
+            <div className="quiz-line"/>
         </div> 
     })
     
     return(
-        <form>
+        <React.Fragment>
             {questionsElements}
-        </form>
+            <button className="quiz-btn">Check Answers</button>
+        </React.Fragment>
     );
 }
